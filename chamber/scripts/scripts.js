59a2833c3474697c7121568d7279fbd3b8c1335f
KCMinchakpu <kevinomics101@gmail.com>
2022-10-06 09:33:41 +0100

    up

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("open");
+	document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
