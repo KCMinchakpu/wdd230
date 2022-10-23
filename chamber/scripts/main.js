8398c27bbfef4d6c77010f12e83aac62407bd9b6
KCMinchakpu <kevinomics101@gmail.com>
2022-10-23 12:10:42 +0100

    up

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -8,3 +8,6 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
+
+const windChillFahrenheit = (temperature, windSpeed) =>
+      35.74 + 0.6215 * temperature -35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed** 0.16;
