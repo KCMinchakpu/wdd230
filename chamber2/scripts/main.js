6506e24e30db48579490621ed7f2461595d01304
KCMinchakpu <kevinomics101@gmail.com>
2022-10-24 23:14:56 +0100

    up

--- chamber2/scripts/main.js
+++ chamber2/scripts/main.js
@@ -1,3 +1,27 @@
-document.querySelector(
-	"#lastModified"
-).textContent = `Last Modification: ${document.lastModified}`;
+/* Primary Nav Button */
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamButton").classList.toggle("open");
+}
+const x = document.getElementById('hamButton')
+x.onclick = toggleMenu;
+
+
+/*JS for date */
+
+document.querySelector(".date").textContent = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(new Date());
+
+
+/*Last Modified */
+document.querySelector('#year').textContent = new Date().getFullYear();
+document.querySelector('#lastupdated').textContent = `Last Modification: ${document.lastModified}`;
+
+
+/*Join Us Banner */
+const day = new Date();
+const today = day.getDay();
+
+if (today < 1 || today > 2) {
+    const banner = document.querySelector('.meeting');
+    banner.remove();
+}  
