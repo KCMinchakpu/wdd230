6bcb0aff1d96caad7859e3e1dece6a23d3396b9b
EliakimCross <octatesystems@gmail.com>
2023-05-13 17:59:58 +0100

    commit

--- lesson3/scripts/main.js
+++ lesson3/scripts/main.js
@@ -1,1 +1,12 @@
 document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
