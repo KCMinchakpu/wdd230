6c5e93aabb5839483162c8089ff7573b63313d26
EliakimCross <octatesystems@gmail.com>
2023-06-15 00:55:45 +0100

    commit

--- lesson09/scripts/filter.js
+++ lesson09/scripts/filter.js
@@ -1,49 +1,24 @@
 
-filterSelection("nofliter")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "nofliter") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" 
-  // class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+function openFilter(evt, filterName) {
+  // Declare all variables
+  var i, tabcontent, filterlinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="filterlinks" and remove the class "active"
+  filterlinks = document.getElementsByClassName("filterlinks");
+  for (i = 0; i < filterlinks.length; i++) {
+    filterlinks[i].className = filterlinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(filterName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-// Show filtered elements
-function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-// Hide elements that are not selected
-function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
