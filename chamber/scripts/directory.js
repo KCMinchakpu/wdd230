5c8a74ce88646eb4351f586f7760c84915109762
KCMinchakpu <min22002@byui.edu>
2023-06-16 13:05:22 +0100

    commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,7 +1,8 @@
 // -----Store the resource, the URL of the JSON file into a const variable--------
 
-const requestURL = "chamber/json/data.json";
-const companies = document.querySelector('.companies');
+const requestURL = 'chamber/json/data.json';
+const cards = document.querySelector('.cards');
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
@@ -9,63 +10,67 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const companies = jsonObject['companies'];
-    companies.forEach(displayCompanies);
+    companies.forEach(Companies);
 });
 
-//   Build the HTML of the Company card using the createElement() and appendChild() methods on the document
-function displayCompanies(company) {
+
+
+function Companies(company) {
     // Create elements to add to the document
-    let company = document.createElement('section');
+    let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let address= document.createElement('p');
     let phoneNumber= document.createElement('p');
     let website= document.createElement('p');
     let level= document.createElement('p');
     let portrait = document.createElement('img');
-
-    // Change the textContent property of the h2 element to contain the company's full name
-    // Change the text Content property of the p element to contain the company's info
+  
+    // Change the textContent property of the h2 element to contain the prophet's full name
     h2.textContent = `${company.name}`;
+  
+   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    portrait.setAttribute('src', company.imageurl);
+    portrait.setAttribute('alt', `Portait of ${company.name}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '225');
+    portrait.setAttribute('height', '225');
+  
+    // Change the text Content property of the p element to contain the prophet's birth date and birth place
     address.textContent = `Address: ${company.address}`;
     phoneNumber.textContent = `Phone Number: ${company.phoneNumber}`;
     website.textContent = `Website: ${company.website}`;
     level.textContent = `Level: ${company.level}`;
-  
-   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-   portrait.setAttribute('src', company.imageurl);
-   portrait.setAttribute('alt', `Portait of ${company.name}`);
-   portrait.setAttribute('loading', 'lazy');
-   portrait.setAttribute('width', '225');
-   portrait.setAttribute('height', '225');
+    
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(address);
+    card.appendChild(phoneNumber);
+    card.appendChild(website);
+    card.appendChild(level);
+    card.appendChild(portrait);
    
-  
-   // Add/append the section(card) with the h2 element
-    company.appendChild(h2);
-    company.appendChild(address);
-    company.appendChild(phoneNumber);
-    company.appendChild(website);
-    company.appendChild(level);
-    company.appendChild(portrait);
-  
     // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('.dirgrid').appendChild(company);
+    document.querySelector('div.cards').appendChild(card);
   }
 
   // TOGGLE DIRECTORY VIEW
-  // Add active class to the current button (highlight it)
-  const display = document.querySelector(".dirgrid");
+ 
+  // Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
 
-  function listView(){
-    display.classList.add("dirlist");
-    display.classList.remove("dirgrid");
-    document.getElementById("listbtn").classList.add("active");
-    document.getElementById("gridbtn").classList.remove("active");
+// Declare a loop variable
+var i;
+
+// List View
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
   }
+}
 
-  function gridView(){
-    display.classList.add("dirgrid");
-    display.classList.remove("dirlist");
-    document.getElementById("gridbtn").classList.add("active")
-    document.getElementById("listbtn").classList.remove("active");
-    
+// Grid View
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
   }
