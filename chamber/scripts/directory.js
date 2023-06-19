67f0532b0d1c41df9771057a69f5a08b5bcf7de2
EliakimCross <octatesystems@gmail.com>
2023-06-19 09:23:45 +0100

    commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,7 +1,7 @@
 // -----Store the resource, the URL of the JSON file into a const variable--------
 
-const requestURL = 'chamber/json/data.json';
-const cards = document.querySelector('.cards');
+const requestURL = 'https://github.com/KCMinchakpu/data/companiesdata.json';
+
 
 fetch(requestURL)
   .then(function (response) {
@@ -9,69 +9,68 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const companies = jsonObject['companies'];
-    companies.forEach(Companies);
-});
+    const businesses = jsonObject['businesses'];
+ 
+    var firstItem = true;
+    for (let i = 0; i < businesses.length; i++ ) {
+      console.log("card #", i,": ", businesses[i].business);
 
 
-
-function Companies(company) {
     // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let address= document.createElement('p');
-    let phoneNumber= document.createElement('p');
-    let website= document.createElement('p');
-    let level= document.createElement('p');
-    let portrait = document.createElement('img');
-  
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${company.name}`;
-  
-   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', company.imageurl);
-    portrait.setAttribute('alt', `Portait of ${company.name}`);
-    portrait.setAttribute('loading', 'lazy');
-    portrait.setAttribute('width', '225');
-    portrait.setAttribute('height', '225');
-  
-    // Change the text Content property of the p element to contain the prophet's birth date and birth place
-    address.textContent = `Address: ${company.address}`;
-    phoneNumber.textContent = `Phone Number: ${company.phoneNumber}`;
-    website.textContent = `Website: ${company.website}`;
-    level.textContent = `Level: ${company.level}`;
+    let card = document.createElement('section'); //card
+    let image = document.createElement('img'); //logo
+    let div = document.createElement('div'); //company info holder
+    let h2 = document.createElement('h2'); //company name
+    let p1 = document.createElement('p'); //email
+    let p2 = document.createElement('p'); //phone number
+    let p3 = document.createElement('p'); //site reference
+    let p4 = document.createElement('p'); //Company membership level
     
+    // Build the image attributes for the src, alt, and loading attribute values. 
+    image.src = String(`${businesses[i].imageurl}`);
+    image.alt = String(`${businesses[i].business} - photo`);
+    image.width = "225";
+    image.height = "225";
+    image.loading = "lazy"
+
+    h1.textContent = String(`${businesses[i].business}`);
+    // Change the text Content property of the p element to contain the company's phonenumber, address, website and level
+    console.log("businesses[i].contactInfo: ", businesses[i].contactInfo.phoneNumber);
+    p1.innerHTML = String(`Phone: ${businesses[i].contactInfo.phoneNumber}`);
+    p2.innerHTML = String(`Address: ${businesses[i].contactInfo.address}`);
+    p3.innerHTML = String(`Website: ${businesses[i].website}`);
+    p4.innerHTML = String(`Level: ${businesses[i].level}`);
+
 
     // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(address);
-    card.appendChild(phoneNumber);
-    card.appendChild(website);
-    card.appendChild(level);
-    card.appendChild(portrait);
+    card.appendChild(image);
+    div.appendChild(h1);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    div.appendChild(p4);
+    card.appendChild(div);
+    card.setAttribute("dataset", businesses[i].url);
    
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
-  }
+   function openLink(e) {
+      e.preventDefault();
+      console.log("e.target: ", e.target.attributes[0]);
+      window.location = e.target.attributes[0].value;
+    }
+    card.onclick = openLink;
+    card.classList.add('article');
+   
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector('#card-layout').appendChild(card);
+  firstItem = false;
+}
+});
 
-  // TOGGLE DIRECTORY VIEW
+  // Toggle directory view
+  function changeDisplayStyle(type) {
+    console.log("type: ", type);
+    document.querySelector('#card-layout').className="";
+    type == "grid" ? document.querySelector('#card-layout').classList.add("card-grid") : document.querySelector('#card-layout').classList.add("card-list");
+  }
+  
  
-  // Get the elements with class="dirgrid"
-var elements = document.getElementsByClassName("dirgrid");
-
-// Declare a loop variable
-var i;
-
-// List View
-function listView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "100%";
-  }
-}
-
-// Grid View
-function gridView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
-  }
-}
